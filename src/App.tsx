5a2f0da6a5578a15102857cb13e11cd6a737ea16
Revoj-a <revojjoever@gmail.com>
2025-10-19 18:46:50 +0800

    Install Chakra UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,7 @@
-import "./App.css";
-import { Box, Button, Heading, Text } from "@chakra-ui/react";
+import { Button } from "@chakra-ui/react";
 
 function App() {
-  return (
-    <Box p={8}>
-      <Heading mb={4}>Welcome to Game Hub</Heading>
-      <Text mb={4}>
-        This is a React + TypeScript + Chakra UI setup using Vite.
-      </Text>
-      <Button colorScheme="teal">Click Me</Button>
-    </Box>
-  );
+  return <Button colorScheme="blue">Button</Button>;
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,11 @@ import { ChakraProvider } from "@chakra-ui/react";
 import "./index.css";
 import App from "./App.tsx";
 import theme from "./theme";
+import { color } from "framer-motion";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ChakraProvider theme={theme}>
+    <ChakraProvider>
       <App />
     </ChakraProvider>
   </StrictMode>
